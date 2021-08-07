d3aff7c1513d0f3b4d62b14b1117b6e553ce3779
FioDev <77973580+FioDev@users.noreply.github.com>
2021-08-07 17:20:43 +0100

    Main cpp now has functional render update and load stuff
    In reality this probably looks more like a scene class than anything, but its a good start i hope

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -1,20 +1,61 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+using namespace sf;
+using namespace std;
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
-    }
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-  return 0;
+//There's probably a more elegant way to do this
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+
+void Reset()
+{
+	//FR i think we should use this as a respawn method - it probably needs moved to another class though
+}
+
+void Load() {
+	// Update Everything
+	
+
+	Reset();
+}
+
+void Update(RenderWindow& window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+
+	// check and consume events
+	Event event;
+
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+	// Quit Via ESC Key - as per usual
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+}
+
+
+//FR MOVE THIS SOMEWHERE ELSE 
+void Render(RenderWindow& window) {
+	// Draw Everything
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "S L I N G S H O T");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+	return 0;
 }
