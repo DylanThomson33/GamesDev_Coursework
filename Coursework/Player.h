3ae3b20c4940741768f5dbfe5023e30bf518f616
FioDev <77973580+FioDev@users.noreply.github.com>
2021-08-08 05:14:38 +0100

    Player is now correct, ill fix the rest
    Messed up my git push again

--- Coursework/Player.h
+++ Coursework/Player.h
@@ -1,3 +1,19 @@
 #pragma once
 //inherits from ship
 //adds score, fuel, etc
+
+#include "Entity.h";
+
+
+class Player : public Entity {
+private:
+	float _acceleration;
+
+public:
+	void Update(double dt) override;
+	Player();
+	void Render(sf::RenderWindow& window) const override;
+	float getDirection();
+	float getSpeed();
+
+};
