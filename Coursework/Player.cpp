d92181bd828c0e8d1a0fa6ec21198727b3239823
FioDev <77973580+FioDev@users.noreply.github.com>
2021-08-09 08:22:40 +0100

    Added player rotation
    Title is descriptive

--- Coursework/Player.cpp
+++ Coursework/Player.cpp
@@ -1,28 +1,35 @@
-
-
 #include "Player.h";
 #include "Global_Values.h";
 #include "Render.h";
+#include "Bullets.h"
 #include <stdlib.h>
-#include "Bullets.h";
 
 using namespace sf;
 using namespace std;
 
-float direction;
+float direction = 0.0f;
 
+
+float velocity = 0.0f;
 float scalarY = 0.0f;
 float scalarX = 0.0f;
 
+//bullet pool
+
 
 Player::Player()
-    : _acceleration(0.5f), Entity(make_unique<sf::Sprite>()) {
+
+    : _acceleration(1.5f), Entity(make_unique<sf::Sprite>()) {
     _sprite->setTexture(spritesheet);
-    _sprite->setTextureRect(IntRect(0, 0, 32, 32));
-    _sprite->setPosition(sf::Vector2f(100, 200));
+    _sprite->setTextureRect(IntRect(160, 32, 32, 32));
+    _sprite->setOrigin(Vector2f(16.0f, 16.0f));
+    _position = Vector2f(500.0f, 500.0f);
 }
+
 float calculateDirection(float x, float y)
-{ 
+{
+    //Hey Dylan, ready to see some cool maths that made me cry at 4:27am this morning?
+
     //temp variables for your viewing pleasure
     float dot;
     float det;
@@ -40,14 +47,12 @@ float calculateDirection(float x, float y)
     return angle;
 }
 
-float getDirection()
+float getPrograde()
 {
-
-    direction = calculateDirection(scalarX, scalarY);
     //now, following that calculate bit you might be wondering why i chose to return the variable through this get
     //its because i live to spite god and couldnt for the life of me get this function to work. i dont know if it was a ghost o 
     // -Fio
-    return direction;
+    return calculateDirection(scalarX, scalarY);;
 }
 
 void Player::Update(double dt) {
@@ -74,53 +79,34 @@ void Player::Update(double dt) {
     {
         scalarX++;
     }
+    if (Keyboard::isKeyPressed(Keyboard::Q))
+    {
+        direction--;
+        _sprite.get()->setRotation(direction);
+    }
+    //move right
+    if (Keyboard::isKeyPressed(Keyboard::E))
+    {
+        direction++;
+        _sprite.get()->setRotation(direction);
+    }
+
 
     //shoot weapons
     if (Keyboard::isKeyPressed(Keyboard::Space)) {
-        scalarY++;
+
+       // vector<Bullets> bulletpool;
+
+      //  bulletpool.push_back(Bullets().Fire(_position.x, _position.y, direction, 0);
     }
 
 
-    //firing bullet 
-    if (Keyboard::isKeyPressed(Keyboard::F))
-    {
-       
-    }
-
-    
-
-    //boundaries
-    //if touching right wall
-    if (_position.x > gameWidth)
-    {
-        Player::move(Vector2f(-16, 0));
-        scalarX = 0;
-    }
-    //if touching left wall
-    if (_position.x < 0)
-    {
-        Player::move(Vector2f(16, 0));
-        scalarX = 0;
-    }
-    //if touching top wall
-    if (_position.y > gameHeight)
-    {
-        Player::move(Vector2f(0, -16));
-        scalarY = 0;
-    }
-    //if touching bottom wall 
-    if (_position.y < 0)
-    {
-        Player::move(Vector2f(0, 16));
-        scalarY = 0;
-    }
 
     Player::move(Vector2f(scalarX * _acceleration * dt, scalarY * _acceleration * dt));
     Entity::Update(dt);
 }
 
-void Player::Render() 
+void Player::Render()
 {
     Renderer::queue(_sprite.get());
 }
-
